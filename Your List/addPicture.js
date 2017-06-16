0130643704e2ec8dd33b6cf4775bf07c372cdead
fedjakova-anastasija <Asya_17@mail.ru>
2017-06-16 15:09:55 +0300

    Add files via upload

---
+++ Your List/addPicture.js
@@ -0,0 +1,99 @@
+function addPicture() {
+  let buttonAdd = document.createElement("input");
+
+  buttonAdd.type = "button";
+  buttonAdd.id = "button_add";
+  buttonAdd.value = "attach";
+  buttonAdd.onclick = img;
+  contentDiv.appendChild(buttonAdd);
+}
+
+function img() {
+  let img = document.createElement('img');
+
+  img.setAttribute('src', 'images/stiker.png');
+  img.id = "id_image";
+  img.classList = "image";
+  movePicture();
+  document.body.appendChild(img);
+  contentDiv.appendChild(img);
+}
+
+function movePicture() {
+
+  document.onmousedown = function (e) {
+
+    let dragElement = e.target;
+
+    if (!dragElement.classList.contains('image')) return;
+
+    let coords;
+    let shiftX;
+    let shiftY;
+
+    startDrag(e.clientX, e.clientY);
+
+    document.onmousemove = function (e) {
+      moveAt(e.clientX, e.clientY);
+    };
+
+    dragElement.onmouseup = function () {
+      finishDrag();
+    };
+
+    function startDrag(clientX, clientY) {
+
+      shiftX = clientX - dragElement.getBoundingClientRect().left;
+      shiftY = clientY - dragElement.getBoundingClientRect().top;
+
+      dragElement.style.position = 'fixed';
+
+      document.body.appendChild(dragElement);
+
+      moveAt(clientX, clientY);
+    };
+
+    function finishDrag() {
+      dragElement.style.top = parseInt(dragElement.style.top) + pageYOffset + 'px';
+      dragElement.style.position = 'absolute';
+
+      document.onmousemove = null;
+      dragElement.onmouseup = null;
+    }
+
+    function moveAt(clientX, clientY) {
+      let newX = clientX - shiftX;
+      let newY = clientY - shiftY;
+
+      //нижняя граница окна
+      let newBottom = newY + dragElement.offsetHeight;
+
+      if (newBottom > document.documentElement.clientHeight) {
+        let docBottom = document.documentElement.getBoundingClientRect().bottom;
+        let scrollY = Math.min(docBottom - newBottom, 10);
+
+        if (scrollY < 0) scrollY = 0;
+        window.scrollBy(0, scrollY);
+        newY = Math.min(newY, document.documentElement.clientHeight - dragElement.offsetHeight);
+      }
+
+      //верхняя граница окна
+      if (newY < 0) {
+        let scrollY = Math.min(-newY, 10);
+        if (scrollY < 0) scrollY = 0;
+
+        window.scrollBy(0, -scrollY);
+        newY = Math.max(newY, 0);
+      }
+      if (newX < 0) newX = 0;
+      if (newX > document.documentElement.clientWidth - dragElement.offsetHeight) {
+        newX = document.documentElement.clientWidth - dragElement.offsetHeight;
+      }
+
+      dragElement.style.left = newX + 'px';
+      dragElement.style.top = newY + 'px';
+    }
+
+    return false;
+  }
+}
